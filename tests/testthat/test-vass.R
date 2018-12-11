e909d339efdf043fe8eb75693ce39891cda9a073
Peter M C Harrison <pmc.harrison@gmail.com>
2018-12-11 13:49:39 +0000

    Adding regression tests for Vassilakis's model

--- tests/testthat/test-vass.R
+++ tests/testthat/test-vass.R
@@ -3,13 +3,9 @@ context("test-vass")
 library(magrittr)
 library(tibble)
 
-test_that("simple categorical examples", {
-  expect_gt(roughness_vass(c(60, 63, 66)),
-            roughness_vass(c(60, 64, 67)))
-})
-
-test_that("Vassilakis (2001, p. 208), ", {
-  f <- tribble(
+test_that("Comparing model outputs to Vassilakis (2001, p. 210)", {
+  # This table comes from p. 208
+  .f <- tribble(
     ~ f1, ~ f2, ~ f3, ~ f4, ~ f5, ~ f6,
     262,  526,  790,  1049, 1318, 1573,
     277,  554,  837,  1118, 1398, 1677,
@@ -25,11 +21,32 @@ test_that("Vassilakis (2001, p. 208), ", {
     494,  990,  1484, 1985, 2476, 2973,
     524,  1052, 1573, 2110, 2634, 3154
   )
-  a <- 1 / 1:6
-  get_tone <- function(pc) as.numeric(f[pc + 1, ])
-  get_tones <- function(pc_1, pc_2) {
-
+  .a <- 1 / 1:6
+  get_tone <- function(pc) {
+    hrep::.fr_sparse_spectrum(frequency = as.numeric(.f[pc + 1, ]),
+                              amplitude = .a)
   }
-  roughness_vass(get_tone(0), a)
-  roughness_vass(get_tone(1), a)
+  get_dyad <- function(pc_1, pc_2) {
+    c(get_tone(pc_1),
+      get_tone(pc_2))
+  }
+  get_dyad_roughness <- function(pc_1, pc_2) {
+    get_dyad(pc_1, pc_2) %>%
+      {roughness_vass(frequency = hrep::freq(.),
+                      amplitude = hrep::amp(.))}
+  }
+
+  # These results come from p. 210
+  res <- tibble(int = 2:12,
+                old = 40.383 /
+                  c(27.617, 18.117, 16.002,
+                    11.446, 12.826, 6.17877,
+                    10.103, 5.782, 6.214,
+                    6.996, 1.589))
+  res$new <- vapply(res$int, function(x) {
+    get_dyad_roughness(0, 1) / get_dyad_roughness(0, x)
+  }, numeric(1))
+  res
+
+  expect_gt(cor(res$old, res$new), 0.998)
 })
